a776e4b49eba792c12a94976b0b6e92fbb9a6244
Danial Arif <danial.arif1309@gmail.com>
2026-08-20 00:44:26 +0000

    Finished off unit 6 in the exponential api :D

--- Exponential_API.py
+++ Exponential_API.py
@@ -15,8 +15,14 @@ class Exponential_Function:
     def get_exponent_laws(self):
         return "(xᵃ)(xᵇ) = xᵃ⁺ᵇ","xᵃ ÷ xᵇ = xᵃ⁻ᵇ","(xᵃ)ᵇ = xᵃˣᵇ","x⁰ = 1","xᵃᐟᵇ = ᵇ√xᵃ = (ᵇ√x)ᵃ","x⁻ᵃ = 1/xᵃ"
 
+    def get_power_law_of_logarithms(self):
+        return "log♭xⁿ = nlog♭x"
+
+    def get_change_of_base_formula(self):
+        return "log♭x = logₐx ÷ logₐ♭"
+
     def subscript_replacer(self,inverted_expo_func):
-            equation = (self.invert_exponential_function.replace("_0","₀_").replace("_1","₁_").replace("_2","₂_").replace("_3","₃_").replace("_4","₄_").replace("_5","₅_").replace("_6","₆_").replace("_7","₇_").replace("_8","₈_").replace("_9","₉_").replace("_-0","₋₀_").replace("_-1","₋₁_").replace("_-2","₋₂_").replace("_-3","₋₃_").replace("_-4","₋₄_").replace("_-5","₋₅_").replace("_-6","₋₆_").replace("_-7","₋₇_").replace("_-8","₋₈_").replace("_-9","₋₉_"))
+            equation = (inverted_expo_func.replace("_0","₀_").replace("_1","₁_").replace("_2","₂_").replace("_3","₃_").replace("_4","₄_").replace("_5","₅_").replace("_6","₆_").replace("_7","₇_").replace("_8","₈_").replace("_9","₉_").replace("_-0","₋₀_").replace("_-1","₋₁_").replace("_-2","₋₂_").replace("_-3","₋₃_").replace("_-4","₋₄_").replace("_-5","₋₅_").replace("_-6","₋₆_").replace("_-7","₋₇_").replace("_-8","₋₈_").replace("_-9","₋₉_"))
             if "_" not in equation:
                 print(equation)
             else:
@@ -36,12 +42,41 @@ class Exponential_Function:
     def invert_exponential_function(self,exponential_func):
         if "^" not in exponential_func:
             exponential_func += "^1"
+        exponential_func = exponential_func.replace(" ","")
         temp_func = "|".join(exponential_func.split("^"))
-        temp_func = "|".join(exponential_func.split("="))
+        temp_func = "|".join(temp_func.split("="))
         temp_func = temp_func.split("|")
         y,b,x = temp_func[0],temp_func[1],temp_func[2]
         inverted_func = self.subscript_replacer(f"{y} = log_{b}") + x
         return inverted_func
+
+    def get_log(self, x):
+        '''tempval = 0
+        while True:
+            if self.b**(tempval) != x:
+                tempval+=1
+            else:
+                break'''
+        return math.log(x,self.b) 
+
+    def get_log_base_10(self,x):
+        return math.log10(x)
+
+    def get_e_value(self):
+        return math.e
+
+    def get_ln(self,x):
+        return math.log(x,self.get_e_value())
+
+    def transform_log_func_coords(self,a=1,k=1,d=0,c=0):
+        xcoords = self.get_expo_func_coords()[0]
+        ycoords = self.get_expo_func_coords()[1]
+        newxcoords = []
+        newycoords = []
+        for i in range(len(xcoords)):
+            newxcoords.append(xcoords[i]/k + d)
+            newycoords.append(ycoords[i]*a + c)
+        return newxcoords,newycoords
 
     def get_expo_func_coords(self):
         ycoords = []
@@ -61,9 +96,9 @@ class Exponential_Function:
     
 
 if __name__ == "__main__":
-    obje = Exponential_Function(1.01)
-    print(obje.get_exponent_laws())
+    obje = Exponential_Function(3)
     obje.graph_expo_function()
+    print(obje.invert_exponential_function("20 = 3^7"))
 
     
         
