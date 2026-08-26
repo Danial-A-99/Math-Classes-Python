3a1879d384a881b081b13950b3e8445b7d056c6a
Danial Arif <danial.arif1309@gmail.com>
2026-08-26 23:35:56 +0000

    added radicals api (originaly from pc in mathematics dan folder for the site) and its rough and needs to be tested

--- tester.py
+++ tester.py
@@ -1,17 +1,29 @@
 from Quadratic_API import Quadratic_Function
-from Trigonometry_API import Trigonometric_Function
+from Trigonometry_API import Trigonometric_Function_Radians
 
-obje = Quadratic_Function(3,4,5)
+#obje = Quadratic_Function(3,4,5)
 
-obje.display_point_slope_form()
-obje.display_root_form()
-obje.display_standard_form()
+#obje.display_point_slope_form()
+#obje.display_root_form()
+#obje.display_standard_form()
 
 from Shape_API import Shape_Function
 
 obje = Shape_Function()
 
-print(obje.cylinder_colume(3,5))
+#print(obje.triangular_prism_volume(1,1,1))
+
+from Linear_API import Linear_Function
+
+myfunc = Linear_Function(-0.1,200,200)
+
+#print(myfunc.graph_linear_function())
+
+from Exponential_API import Exponential_Function
+
+expfunc = Exponential_Function(-1.08)
+
+expfunc.graph_expo_function()
 
 
 
